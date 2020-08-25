92af90d47b9f951071ebc6193e57159628ff1bba
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-08-24 17:44:28 -0700

    adds signin and signup to front end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch,
+} from 'react-router-dom';
+import SignIn from './Auth/SignIn.js';
+import Signup from './Auth/Signup.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    token: localStorage.getItem('token'),
+  }
+
+  handleToken = (token) => {
+    this.setState({ token: token })
+
+    localStorage.setItem('token', token)
+  }
+
+  clearToken = () => {
+    this.setState({ token: ''})
+
+    localStorage.setItem('token', '')
+  }
+
+  render() { 
+    return (
+      <body>
+            <Router>
+                <Switch>
+                    <Route 
+                        path="/" 
+                        exact
+                        render={(routerProps) => <SignIn handleToken={this.handleToken} token={this.state.token} {...routerProps} />} 
+                    />
+                    <Route 
+                        path="/signup" 
+                        exact
+                        render={(routerProps) => <Signup handleToken={this.handleToken} token={this.state.token} {...routerProps} />} 
+                    />
+                </Switch>
+            </Router>
+        </body>
+    )
+  }
 }
 
 export default App;
