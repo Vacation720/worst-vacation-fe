8f67b71448854164a7360e641be0687ab0950af2
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-08-26 14:30:20 -0700

    adds post route and labels

--- src/CreateItinerary/CreateItinerary.js
+++ src/CreateItinerary/CreateItinerary.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Hotels from './Hotels.js';
-import Restaurant from './Restaurant.js'
-import Bar from './Bar.js'
+// import Restaurant from './Restaurant.js'
+// import Bar from './Bar.js'
 // import Tattoo from './Tattoo.js'
 // import Attraction from './Attraction.js'
 
 class CreateItinerary extends React.Component {
     state = {
         lat: this.props.location.state.lat,
-        lon: this.props.location.state.lon
+        lon: this.props.location.state.lon,
+        trip_id: 1
     }
 
     render() { 
         return (
             <div>
-                <Hotels lat={ this.state.lat } lon={ this.state.lon }/>
-                <Bar lat={ this.state.lat } lon={ this.state.lon }/>
+                <Hotels trip_id={this.state.trip_id} lat={ this.state.lat } lon={ this.state.lon }/>
+                {/* <Bar lat={ this.state.lat } lon={ this.state.lon }/> */}
                 {/* <Tattoo lat={ this.state.lat } lon={ this.state.lon }/> */}
                 {/* <Attraction lat={ this.state.lat } lon={ this.state.lon }/> */}
-                <Restaurant lat={ this.state.lat } lon={ this.state.lon }/>
+                {/* <Restaurant lat={ this.state.lat } lon={ this.state.lon }/> */}
             </div>
         );
     }
