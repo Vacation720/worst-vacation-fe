88a66f4cc021f4cd1857a5c7a9b6e565b166b671
internetcharles <charlesodellsmith@gmail.com>
2020-08-27 15:43:26 -0700

    fixed itinerary bug

--- src/Itineraries/Itineraries.js
+++ src/Itineraries/Itineraries.js
@@ -19,9 +19,14 @@ class Itineraries extends React.Component {
     handleDelete = async (id) => {
         await deleteTrip(id);
 
-        const data = await getChoices(this.props.token)
-    
-        this.setState({ tripItem: data.body })
+        const tripData = await getChoices();
+        let arr = [];
+
+        for(let i = 0; i < tripData.body.length / 5; i++) {
+            arr.push(tripData.body.slice(i * 5, i * 5 + 5));
+        }
+
+        this.setState({ tripItem: arr})
         }
     
     render() {
@@ -38,6 +43,10 @@ class Itineraries extends React.Component {
                                         return (
                                             <>
                                                 <div>{item.business_name}</div>
+                                                {item.image_url ? <img src={item.image_url} alt={item.business_name}/> : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png" alt="Not Found" />}
+                                                <div>RATING: {item.rating}</div>
+                                                <div>"{item.review}"</div>
+                                                <div>{item.address ? item.address : "Unknown"}, {item.city}</div>                                            
                                             </>
                                         )
                                     })
