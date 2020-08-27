74aa91df6514c82870e9a2a82d82d7f1e9131219
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-08-27 10:18:23 -0700

    continuing to work on the selection css

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -37,7 +37,7 @@ class Home extends React.Component {
                   }} />
             }
         return (
-            <main>
+            <main className='home-main'>
                 <form onSubmit={this.handleSearch} className="form-submit">
                     <input placeholder="Where are you going?" onChange={(e) => this.setState({ search: e.target.value })} value={this.state.search} />
                     <button type="submit">Submit</button>
